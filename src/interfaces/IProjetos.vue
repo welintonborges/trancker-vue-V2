68490559b104e02ccd276ae1709a6cc03a0fb858
p_920763 <welinton.borges@camara.leg.br>
2024-05-28 11:15:43 -0300

    Ajustando a classe de Projetos.vue

---
+++ src/interfaces/IProjetos.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+export default interface IProjetos {
+  id: string;
+  nome: string;
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -1,15 +1,60 @@
 <script  lang="ts">
 import { defineComponent } from 'vue';
+import IProjetos from "@/interfaces/IProjetos.vue";
 
 export default defineComponent({
   name: 'Projetos',
+  data() {
+      return{
+        nomeDoProjeto: "",
+        projetos: [] as IProjetos[]
+      };
+  },
+  methods:{
+    salvar(){
+      const projeto: IProjetos ={
+        nome: this.nomeDoProjeto,
+        id: new Date().toISOString(),
+      }
+      this.projetos.push(projeto);
+      this.nomeDoProjeto = "";
+    }
+  }
 })
 </script>
 
 <template>
-<h1>Projetos</h1>
+  <section class="projetos">
+    <h1 class="title">Projetos</h1>
+
+  <form @submit.prevent="salvar">
+    <div class="field">
+      <label for="nomeDoProjeto" class="label">Nome do Projeto</label>
+      <input type="text" id="nomeDoProjeto" class="input" v-model="nomeDoProjeto"  />
+    </div>
+    <div class="field">
+      <button class="button" type="submit">Salvar</button>
+    </div>
+  </form>
+  <table class="table is-fullwidth">
+    <thead>
+    <tr>
+      <th>ID</th>
+      <th>Nome</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="projeto in projetos" :key="projeto.id">
+      <td>{{ projeto.id }}</td>
+      <td>{{ projeto.nome }}</td>
+    </tr>
+    </tbody>
+  </table>
+</section>
 </template>
 
 <style scoped>
-
+.projetos {
+  padding: 1.25rem;
+}
 </style>
